c2d6bc077003cdb8c3197955efd8109ecdef7c52
HFZR2005 <100490433+HFZR2005@users.noreply.github.com>
2022-08-07 22:18:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def addVectors(angle1, length1, angle2, length2):
 
 gravity = 0.02
 gravity_angle = math.pi
-
+elasticity = 0.999
 
 
 #---------------------------------------------------- mouse interactions
@@ -34,14 +34,49 @@ def findParticle(particles, x, y):
             return p
     return None
 
+#---------------------------------------------------- collisions
+
+def collide(p1, p2):
+    dx = p1.x - p2.x
+    dy = p1.y - p2.y
+
+    if math.hypot(dx, dy) < p1.size + p2.size:
+        angle = math.atan2(dy, dx) + 0.5 * math.pi
+        total_mass = p1.mass + p2.mass
+        dist = math.hypot(dx, dy)
+
+        a1 = p1.angle
+        a2 = angle
+        a3 = p2.angle
+        a4 = angle + math.pi
+
+        l1 = p1.speed * (p1.mass-p2.mass)/total_mass
+        l2 = 2 * p2.speed * p2.mass/total_mass
+        l3 = p2.speed * (p2.mass-p1.mass)/total_mass
+        l4 = 2 * p1.speed*p1.mass/total_mass
+
+        p1.angle, p1.speed = addVectors(a1, l1, a2, l2)
+        p2.angle, p2.speed = addVectors(a3, l3, a4, l4 )
+
+        p1.speed *= elasticity
+        p2.speed *= elasticity
+
+        overlap = 0.5 * (p1.size + p2.size - dist + 1)
+        p1.x += math.sin(angle) * overlap
+        p1.y -= math.cos(angle) * overlap
+        p2.x -= math.sin(angle) * overlap
+        p2.y += math.cos(angle) * overlap
+
+
 
 #----------------------------------------------------------------------------- creating a particle class
 
 class Particle():
-    def __init__(self, x, y, size):
+    def __init__(self, x, y, size, mass = 1):
         self.x = x
         self.y = y
         self.size = size
+        self.mass = mass
         self.colour = (0, 0, 255)
         self.thickness = 1
         self.speed = 0
@@ -74,18 +109,22 @@ class Particle():
             self.y = 2 * self.size - self.y
             self.angle = math.pi - self.angle
 
+        self.speed *= elasticity
+
 #----------------------------------------------------------------------------- placing random particles
 
-number_of_particles = 10
+number_of_particles = 5
+
 my_particles = []
 
 
 for n in range(number_of_particles):
     size = random.randint(10, 20)
+    density = random.randint(1, 20)
     x = random.randint(size, width-size)
     y = random.randint(size, height-size)
 
-    particle = Particle(x, y, size)
+    particle = Particle(x, y, size, density * size ** 2)
     particle.speed = random.random()
     particle.angle = random.uniform(0, math.pi*2)
 
@@ -116,8 +155,10 @@ while running:
 
     screen.fill(bg)
 
-    for particle in my_particles:
+    for i, particle in enumerate(my_particles):
         particle.move()
         particle.bounce()
+        for particle2 in my_particles[i + 1:]:
+            collide(particle, particle2)
         particle.display()
     pygame.display.flip()
